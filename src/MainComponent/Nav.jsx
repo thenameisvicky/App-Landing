f79f62bb5f7738f2c27a32c4fd069e13a4903b49
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2024-08-06 11:30:18 +0530

    Made a sidebar to popup

--- src/MainComponent/Nav.jsx
+++ src/MainComponent/Nav.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
 import { navLinks } from '../Constants';
 
 const Nav = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <header className="px-4 py-4 md:px-8 md:py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-w-7xl mx-auto">
@@ -15,9 +26,30 @@ const Nav = () => {
           ))}
         </ul>
         <div className="block md:hidden">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
+          <img src={hamburger} alt="hamburger" width={25} height={25} onClick={toggleSidebar} />
         </div>
       </nav>
+
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 right-0 h-full bg-white w-1/2 transform ${
+          isSidebarOpen ? 'translate-x-0' : 'translate-x-full'
+        } transition-transform duration-300 ease-in-out z-20`}
+      >
+        <div className="flex flex-col items-start p-4">
+          <button onClick={toggleSidebar} className="self-end mb-4">Close</button>
+          {navLinks.map((item) => (
+            <a
+              key={item.label}
+              className="text-slate-gray font-montserrat leading-normal text-lg underline-link py-2"
+              href={item.href}
+              onClick={handleLinkClick}
+            >
+              {item.label}
+            </a>
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
