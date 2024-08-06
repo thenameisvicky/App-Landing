8108168e69a50cc5ca6f417c1a4355c70735c2bc
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2024-08-06 11:03:15 +0530

    Made responsive button

--- src/MainComponent/Button.jsx
+++ src/MainComponent/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({
 }) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      className={`flex justify-center items-center gap-2 px-4 py-2 md:px-7 md:py-4 border font-montserrat text-base md:text-lg leading-none
       ${
         backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
@@ -16,12 +16,11 @@ const Button = ({
       } rounded-full ${fullWidth && "w-full"} hover:scale-105 transform duration-300 `}
     >
       {label}
-
       {iconURL && (
         <img
           src={iconURL}
-          alt='arrow right icon'
-          className='ml-2 rounded-full bg-white w-5 h-5'
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-4 h-4 md:w-5 md:h-5"
         />
       )}
     </button>
