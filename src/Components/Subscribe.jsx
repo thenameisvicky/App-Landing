15af0bce9f346d038f3fa629394bb8406eec2182
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2024-08-06 11:07:49 +0530

    Made the subscribe responsive

--- src/Components/Subscribe.jsx
+++ src/Components/Subscribe.jsx
@@ -2,16 +2,22 @@ import Button from "../MainComponent/Button";
 
 const Subscribe = () => {
   return (
-    <section className="px-4 md:px-8 mt-12 md:mt-16 max-w-7xl mx-auto">
-      <div className="flex flex-col lg:flex-row justify-between items-center gap-10">
-        <h3 className="text-3xl md:text-4xl leading-normal lg:max-w-md font-palanquin font-bold">
-          Sign Up for <span className="text-coral-red">Updates</span> & Newsletter
-        </h3>
-        <div className="lg:max-w-lg w-full flex items-center gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
-          <input type="text" placeholder="subscribe@nike.com" className="input border-none" />
-          <div className="flex max-sm:justify-end items-center max-sm:w-full">
-            <Button label="Sign Up" fullWidth />
-          </div>
+    <section
+      id="contact-us"
+      className="max-container flex flex-col lg:flex-row justify-between items-center gap-10"
+    >
+      <h3 className="text-3xl md:text-4xl leading-snug md:leading-[68px] font-palanquin font-bold text-center lg:text-left">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className="w-full lg:max-w-[40%] flex flex-col sm:flex-row items-center gap-5 p-2.5 border border-slate-gray rounded-full">
+        <input
+          type="text"
+          placeholder="subscribe@nike.com"
+          className="flex-1 w-full text-base leading-normal text-slate-gray pl-5 py-2.5 border-none outline-none rounded-full"
+        />
+        <div className="w-full sm:w-auto">
+          <Button label="Sign Up" fullWidth />
         </div>
       </div>
     </section>
